3d9f4161965d9d870220f56efe5811a53b1a8aca
Ethan Sienes <crisostomo.eljohn2001@gmail.com>
2026-06-15 11:06:54 +0800

    feat: add openrelay TURN servers to WebRTC configuration in DuetView

--- src/views/DuetView.ts
+++ src/views/DuetView.ts
@@ -918,7 +918,23 @@ export class DuetView implements AppView {
     const configuration = {
       iceServers: [
         { urls: 'stun:stun.l.google.com:19302' },
-        { urls: 'stun:stun1.l.google.com:19302' }
+        { urls: 'stun:stun1.l.google.com:19302' },
+        { urls: 'stun:openrelay.metered.ca:80' },
+        {
+          urls: 'turn:openrelay.metered.ca:80',
+          username: 'openrelayproject',
+          credential: 'openrelayproject'
+        },
+        {
+          urls: 'turn:openrelay.metered.ca:443',
+          username: 'openrelayproject',
+          credential: 'openrelayproject'
+        },
+        {
+          urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+          username: 'openrelayproject',
+          credential: 'openrelayproject'
+        }
       ]
     };
 
